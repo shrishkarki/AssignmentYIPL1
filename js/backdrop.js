3d822b94b90feb08f43336536933f11248ded425
unknown <shrishkarki6669@gmail.com>
2023-06-28 18:17:22 +0545

    added animation to ham icon

--- js/backdrop.js
+++ js/backdrop.js
@@ -1,18 +1,35 @@
 let checkbox=document.getElementById("toggle");
 let backdrop=document.getElementById("backdrop");
+const hamIcons=document.querySelectorAll('.ham-icon');
 
 
-checkbox.addEventListener('click',()=>{
-    
+
+const toggleWithTransition=()=>{
     document.body.classList.toggle('overflow-hidden');
     backdrop.classList.toggle('backdrop-display');
+
+    // for x icon
+
+    if(checkbox.checked){
+        hamIcons[0].style.transform="translateY(4.5px) rotate(45deg)";
+        hamIcons[1].style.transform="scale(0)";
+      
+        hamIcons[2].style.transform="translateY(-15px) rotate(-45deg)";
+    }
+    else{
+        hamIcons.forEach((item,index)=>{
+            item.style.transform="";
+        })
+    }
     
-});
+}
+
+checkbox.addEventListener('click',toggleWithTransition);
+
 backdrop.addEventListener('click',()=>{
 
     checkbox.checked=false;
-    backdrop.classList.toggle('backdrop-display');
-    document.body.classList.toggle('overflow-hidden');
+   toggleWithTransition();
    
     
 });
