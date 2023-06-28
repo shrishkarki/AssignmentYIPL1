75d8f2ef65c1887159852de7d837de6d272df57f
unknown <shrishkarki6669@gmail.com>
2023-06-28 10:14:24 +0545

    added feedback

--- js/carousel.js
+++ js/carousel.js
@@ -2,6 +2,19 @@ const feedback=document.querySelector('.feedback');
 const feedbackItems=document.querySelectorAll('.feedback-item');
 const prevButton=document.getElementById('leftBtn');
 const nextButton=document.getElementById('rightBtn');
+const dots=document.querySelectorAll('.carousel-buttons-dots');
+
+console.log(dots,feedbackItems);
+
+
+
+
+// let itemWidth=feedbackItems[0].offsetWidth;
+// console.log(itemWidth);
+feedbackItems.forEach((element,index) => {
+    element.style.transform=`translateX(${index * 100}vw )`;
+});
+
 
 
 
@@ -10,13 +23,31 @@ let currentIndex=0;
 
 const totalItems=feedbackItems.length;
 
+
+
+
+
+
 const showItem=(index)=>{
+
+console.log(index);
 if(index < 0 || index >= totalItems){
     return;
 }
 
-const itemWidth=feedbackItems[0].offsetWidth;
-feedback.style.transform=`translateX(-${index * itemWidth}px)`;
+
+// console.log(itemWidth,"hello world");
+
+// feedbackItems.forEach((element,index) => {
+//     element.style.transform=`translateX(-${index * itemWidth}px )`;
+// });
+
+// feedbackItems[index].style.transform=`translateX(-${index * itemWidth}px )`;
+// let itemWidth=feedbackItems[0].offsetWidth;
+// feedback.style.transform=`translateX(-${index * itemWidth}px )`;
+
+feedback.style.transform=`translateX(-${index * 100}vw )`;
+
 currentIndex=index;
 }
 
@@ -38,6 +69,13 @@ const prevItem=()=>{
 
     showItem(currentIndex);
 }
+
+
+
+dots.forEach((item,index)=>{
+    console.log("clickkkk me");
+    item.addEventListener('click',showItem(index));
+})
 
     
 
